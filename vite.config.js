24ea1aa25ffdddc19922273fa17b7dad88b7ec66
Alejandro Martinez <alejandro.martinez@bigcommerce.com>
2025-12-10 06:30:49 -0500

    feat(): Install Shadcn to project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  /* Plugins */
   plugins: [
     react(),
     tailwindcss(),
   ],
+  /* Paths */
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
